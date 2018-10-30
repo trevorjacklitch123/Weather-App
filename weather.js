61b45a44250eae830b032eb08bafa6083f6a39fd
Trevor Jacklitch <trevorjacklitch123@gmail.com>
2018-10-30 18:54:30 -0500

    Got the ipapi and owmapi functionality working

--- weather.js
+++ weather.js
@@ -1,26 +1,26 @@
 let ipApiResponse = {};
-let zipCode = "";
 let OWMApiResponse = {};
+
 fetch("http://ip-api.com/json")
-    .then(response => {
-        return response.json();
-    })
-    .then(responseJson => {
-        ipApiResponse = responseJson;
-        zipCode = responseJson.zip;
-    })
-    .then(OWMApiCall(zipCode));
+    .then((response) => {
+        response.json().then((json) => {
+            ipApiResponse = json;
+            OWMApiCall(ipApiResponse.zip);
+        })
+    });
 
 
-function OWMApiCall(){
+function OWMApiCall(zip){
 /*  
-    OWM means OpenWeatherMap
-    This OWM API call only works for people in the US. Can be changed later.
+    OWM means OpenWeatherMap.
+    This OWM API call only works for people in the US. Can be changed later to include other countries?
 */
+console.log(ipApiResponse.zip);
+console.log(zip);
 const OWMBaseUrl = "http://api.openweathermap.org/data/2.5/weather?";
 //  OWMAPIKey will never be pushed onto github.
-const OWMAPIKey = "";
-const apiRequestURL = `${OWMBaseUrl}zip=${zipCode},US&APPID=${OWMAPIKey}`;
+const OWMAPIKey = "736d3d373c597ed144ecdfc6e96c2af4";
+const apiRequestURL = `${OWMBaseUrl}zip=${zip},US&APPID=${OWMAPIKey}`;
 
 fetch(apiRequestURL)
     .then(response => {
